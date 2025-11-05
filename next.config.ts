96b34e57526a6ce068e34d453ae245ce4c79313a
willsather <will@sather.ws>
2025-11-05 16:23:04 -0500

    chore: remove redirect

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://sitecore-jeopardy.vercel.app",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
